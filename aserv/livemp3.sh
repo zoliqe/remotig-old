0cc9369fc3504d3dd2c80689eb0f4c96687e8d81
Michal Karas <michal.karas@posam.sk>
2018-12-09 21:35:31 +0100

    livemp3 filters

--- aserv/livemp3.sh
+++ aserv/livemp3.sh
@@ -3,6 +3,7 @@
 # run streaming service in background - to access process console use "tmux attach-session"
 tmux new-session -d -s remotig-audio "ffmpeg -y -f alsa -i plug:dsnoop -rtbufsize 64 -probesize 64 \
 -acodec libmp3lame -ab 8k -out_sample_rate 8k -ac 1 -reservoir 0 -f mp3 \
+-af 'lowpass=f=2000, highpass=f=300' \
 -fflags +nobuffer - \
 | node stdinstreamer.js -port 9601 -type mp3 -burstsize 1"
 
