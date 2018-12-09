8e80de7fbe5c72ba46a715a0582c61588e5baff4
Michal Karas <michal.karas@posam.sk>
2018-12-09 20:49:29 +0100

    improve livemp3

--- aserv/livemp3.sh
+++ aserv/livemp3.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ffmpeg -y -f alsa -i plug:dsnoop -rtbufsize 64 -probesize 64 \
--acodec libmp3lame -ab 16k -ac 1 -reservoir 0 -f mp3 \
+# run streaming service in background - to access process console use "tmux attach-session"
+tmux new-session -d -s remotig-audio "ffmpeg -y -f alsa -i plug:dsnoop -rtbufsize 64 -probesize 64 \
+-acodec libmp3lame -ab 8k -out_sample_rate 8k -ac 1 -reservoir 0 -f mp3 \
 -fflags +nobuffer - \
-| node stdinstreamer.js -port 9601 -type mp3 -burstsize 1
+| node stdinstreamer.js -port 9601 -type mp3 -burstsize 1"
+
